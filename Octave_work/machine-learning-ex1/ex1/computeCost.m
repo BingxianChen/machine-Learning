80220557ebd7bc904777dc1f2466cb02a1f11ffb
Uzumaki-C <15951858526@163.com>
2017-03-17 19:39:30 +0800

    单元线性回归

--- Octave_work/machine-learning-ex1/ex1/computeCost.m
+++ Octave_work/machine-learning-ex1/ex1/computeCost.m
@@ -13,7 +13,8 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
+C = X*theta - y;
+J = (C'*C)/(2*m);
 
 
 
--- Octave_work/machine-learning-ex1/ex1/gradientDescent.m
+++ Octave_work/machine-learning-ex1/ex1/gradientDescent.m
@@ -16,8 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
+    copy_theta = theta;
+    theta(1,1) = copy_theta(1,1) - (alpha/m)*((X*copy_theta - y)'*X(:,1));
+    theta(2,1) = copy_theta(2,1) - (alpha/m)*((X*copy_theta - y)'*X(:,2));
 
 
 
